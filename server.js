ac891c522b86bf05b19bf3828a149cf94410edfc
hathairat163 <s0614091051@gmail.com>
2020-10-03 14:47:53 +0700

    work

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+app.get("/",(req,res) => {
+    res.json({message: "Welcome to application."});
+});
+
+require("./app/routes/tutorial.routes")(app)
+app.listen(8080,() => {
+    console.log("Server start at port 8080");
+});
